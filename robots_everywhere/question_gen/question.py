8306857d44ce757cc887500bbbb1fa0e2822cb05
edwinsteenkamer <36700301+edwinsteenkamer@users.noreply.github.com>
2021-03-08 10:28:33 +0100

    Added class Question

---
+++ robots_everywhere/question_gen/question.py
@@ -0,0 +1,52 @@
+"""
+Project Robots Everywhere (TU Eindhoven) Q3 2020-2021 Group 8
+Copyright (C) 2021  Edwin Steenkamer
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Class Question that generates a question
+"""
+from robots_everywhere.database.database_writer import Variable
+
+class Question:
+
+    def __init__(self, content: str, variable: Variable, id: int):
+        self.__content = content
+        self.__variable = variable
+        self.__id = id
+
+    @property
+    def content(self) -> str:
+        return self.__content
+
+    @property
+    def variable(self) -> Variable:
+        return self.__variable
+
+    @property
+    def id(self) -> int:
+        return self.__id
+
+    def __repr__(self) -> str:
+        return f"Question({repr(self.content)},{repr(self.variable)},{repr(self.id)})"
+
+    def __str__(self) -> str:
+        return f"Question({self.id},{repr(self.variable)}):{self.content}"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Question):
+            return False
+        else:
+            return other.content == self.content and other.variable == self.variable and other.id == self.id
---
+++ robots_everywhere/question_gen/test_question_gen/test_question.py
@@ -0,0 +1,83 @@
+"""
+Project Robots Everywhere (TU Eindhoven) Q3 2020-2021 Group 8
+Copyright (C) 2021  Edwin Steenkamer
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Testcases for class Question in question.py
+"""
+import unittest
+from robots_everywhere.question_gen.question import Question
+from robots_everywhere.database.database_writer import Variable
+
+
+class QuestionTestCase(unittest.TestCase):
+
+    def test_repr_1(self):
+        """
+        Base case: str-typed Question
+        """
+
+        question = Question("TestQuestion",Variable(str, "TestVar"),0)
+        expected = "Question('TestQuestion',Variable(str,'TestVar'),0)"
+        result = repr(question)
+        self.assertEqual(result, expected)
+
+    def test_repr_2(self):
+        """
+        Base-case: str-typed Question
+        """
+
+        question = Question("TestQuestion1",Variable(str, "TestVar1"),0)
+        expected = "Question(0,Variable(str,'TestVar1')):TestQuestion1"
+        result = str(question)
+        self.assertEqual(result, expected)
+
+    def test__eq__1(self):
+        """
+        Both inputs should be equal.
+        """
+        que_1 = Question("TestQuestion1",Variable(str, "TestVar1"),0)
+        que_2 = Question("TestQuestion1",Variable(str, "TestVar1"),0)
+        self.assertEqual(que_1, que_2)
+
+    def test__eq__2(self):
+        """
+        Content different.
+        """
+        que_1 = Question("TestQuestion1",Variable(str, "TestVar1"),0)
+        que_2 = Question("TestQuestion2",Variable(str, "TestVar1"),0)
+        self.assertNotEqual(que_1, que_2)
+
+    def test__eq__3(self):
+        """
+        Variable different.
+        """
+        que_1 = Question("TestQuestion1",Variable(str, "TestVar1"),0)
+        que_2 = Question("TestQuestion1",Variable(str, "TestVar2"),0)
+        self.assertNotEqual(que_1, que_2)
+
+    def test__eq__4(self):
+        """
+        Id different.
+        """
+        que_1 = Question("TestQuestion1",Variable(str, "TestVar1"),0)
+        que_2 = Question("TestQuestion1",Variable(str, "TestVar1"),1)
+        self.assertNotEqual(que_1, que_2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
