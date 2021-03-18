5c0b5ba5e6a830b71c750c002709d64aa6a5b26a
nifrec <lulof.piree@zoho.com>
2021-03-18 10:38:17 +0100

    Add more testcases to OutputInvoker

--- robots_everywhere/output/test_output/test_output_invoker.py
+++ robots_everywhere/output/test_output/test_output_invoker.py
@@ -105,6 +105,104 @@ class OuputInvokerTestCase(unittest.TestCase):
         self.assertEqual(result.text, "[14.1]")
         self.assertFalse(self.rule_1.check_fireable(self.db))
 
+    def test_find_fireable_rules_nothing_fireable(self):
+        """
+        Corner case: no rules are fireable.
+        """
+        # This makes the trigger of rule_1 evaluate to False:
+        self.db.insert_new_value_of_var(self.var_2, 99999., 6)
+
+        invoker = OutputInvoker([self.rule_1, self.rule_2], None, self.db)
+        result = invoker._find_fireable_rules()
+
+        self.assertFalse(self.rule_1.check_fireable(self.db),
+                        msg="Testcase broken")
+        self.assertFalse(self.rule_2.check_fireable(self.db),
+                         msg="Testcase broken")
+
+        self.assertSequenceEqual(result, [])
+
+    def test_mainloop_no_output(self):
+        """
+        Run the OutputInvoker one step.
+        No rule should be fireable, so no output is expected.
+        """
+        # This makes the trigger of rule_2 evaluate to False:
+        self.db.insert_new_value_of_var(self.var_2, 99999., 6)
+
+        invoker_end, testcase_end = multiprocessing.Pipe(True)
+        invoker = OutputInvoker([self.rule_1, self.rule_2],
+                                invoker_end, self.db)
+
+        invoker.mainloop(0.001, 1)
+
+        self.assertFalse(testcase_end.poll())
+
+    def test_mainloop_2_outputs(self):
+        """
+        Run the OutputInvoker one step.
+        Both rules should fire during the step.
+        Order of the messages does not matter.
+        Uses different db state than other tests.
+        """
+        
+        db = self.construct_db_both_rules_fireable()
+        invoker_end, testcase_end = multiprocessing.Pipe(True)
+        invoker = OutputInvoker([self.rule_1, self.rule_2],
+                                invoker_end, db)
+
+        invoker.mainloop(0.001, 1)
+
+        self.assertTrue(testcase_end.poll())
+        result_1 = testcase_end.recv()
+        self.assertIsInstance(result_1, OutputMessage)
+
+        self.assertTrue(testcase_end.poll())
+        result_2 = testcase_end.recv()
+        self.assertIsInstance(result_2, OutputMessage)
+
+        self.assertFalse(testcase_end.poll())
+
+    def test_find_fireable_rules_2_fireable(self):
+        """
+        Both rules are fireable.
+        """
+        db = self.construct_db_both_rules_fireable()
+
+        invoker = OutputInvoker([self.rule_1, self.rule_2], None, db)
+        result = invoker._find_fireable_rules()
+
+        self.assertTrue(self.rule_1.check_fireable(db),
+                        msg="Testcase broken")
+        self.assertTrue(self.rule_2.check_fireable(db),
+                         msg="Testcase broken")
+
+        self.assertSequenceEqual(result, [self.rule_1, self.rule_2])
+
+    def construct_db_both_rules_fireable(self) -> DatabaseWriter:
+        """
+        Construct a DatabaseWriter plus corresponding database
+        in which both self.rule_1 and self.rule_2 are fireable.
+        """
+        remove_database(TEST_DB_NAME)
+        db = DatabaseWriter(TEST_DB_NAME)
+
+        db.create_new_var(self.var_1)
+        db.insert_new_value_of_var(self.var_1, 999, 1)
+        db.insert_new_value_of_var(self.var_1, 11, 2)
+        db.insert_new_value_of_var(self.var_1, 12, 3)
+        db.insert_new_value_of_var(self.var_1, 13, 4)
+        db.insert_new_value_of_var(self.var_1, 999, 5)
+
+        db.create_new_var(self.var_2)
+        db.insert_new_value_of_var(self.var_2, 10.1, 1)
+        db.insert_new_value_of_var(self.var_2, 11.1, 2)
+        db.insert_new_value_of_var(self.var_2, 12.1, 3)
+        db.insert_new_value_of_var(self.var_2, 13.1, 4)
+        db.insert_new_value_of_var(self.var_2, 14.1, 5)
+        return db
+
+    
 
 if __name__ == "__main__":
     unittest.main()
