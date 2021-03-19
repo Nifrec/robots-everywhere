dad3205dbee838147a60024e9ff32edc74df66a3
nifrec <lulof.piree@zoho.com>
2021-03-19 22:10:25 +0100

    Workaround inheristance bugs

--- robots_everywhere/message.py
+++ robots_everywhere/message.py
@@ -24,9 +24,9 @@ from robots_everywhere.database.database import Variable
 
 class Message(abc.ABC):
 
-    def __init__(self, id: int):
-        assert isinstance(id, int)
-        self.__id = id
+    def __init__(self, id_num: int):
+        assert isinstance(id_num, int), f"ID must be int, got {type(id_num)}"
+        self.__id = id_num
 
     @property
     def id(self) -> int:
@@ -37,8 +37,8 @@ class VariableMessage(Message):
     Message containing a reference to a Variable.
     """
 
-    def __init__(self, id: int, var: Variable):
-        super().__init__(id)
+    def __init__(self, id_num: int, var: Variable):
+        super().__init__(id_num)
         assert isinstance(var, Variable)
         self.__var = var
 
@@ -46,14 +46,14 @@ class VariableMessage(Message):
     def variable(self) -> Variable:
         return self.__var
 
-class TextMessage():
+class TextMessage(Message):
     """
     Message with a string.
     Can be used to package a text message to send to the user.
     """
 
-    def __init__(self, id: int, text: str):
-        super().__init__(id)
+    def __init__(self, id_num: int, text: str):
+        super().__init__(id_num)
         assert isinstance(text, str)
         self.__text = text
 
@@ -62,16 +62,22 @@ class TextMessage():
     def text(self) -> str:
         return self.__text
 
-class QuestionMessage(TextMessage, VariableMessage):
+class QuestionMessage(VariableMessage):
     
-    def __init__(self, id: int, text: str, var: VariableMessage):
-        TextMessage.__init__(id, text)
-        VariableMessage.__init__(id, var)
+    def __init__(self, id_num: int, text: str, var: VariableMessage):
+        VariableMessage.__init__(self, id_num, var=var)
+        assert isinstance(text, str)
+        self.__text = text
+
+
+    @property
+    def text(self) -> str:
+        return self.__text
 
 class AnswerMessage(VariableMessage):
     
-    def __init__(self, id: int, var: Variable, value: Any):
-        super().__init__(id, var)
+    def __init__(self, id_num: int, var: Variable, value: Any):
+        super().__init__(id_num, var)
         self.__value = value
     
     @property
