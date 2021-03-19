b63473fe9a237c517862a7b28fbf11b214638a43
nifrec <lulof.piree@zoho.com>
2021-03-19 11:57:39 +0100

    Make Question hashable

--- robots_everywhere/question_gen/question.py
+++ robots_everywhere/question_gen/question.py
@@ -46,3 +46,6 @@ class Question:
             return False
         else:
             return other.content == self.content and other.variable == self.variable
+
+    def __hash__(self):
+        return hash(self.__content) + hash(self.__variable.name)
